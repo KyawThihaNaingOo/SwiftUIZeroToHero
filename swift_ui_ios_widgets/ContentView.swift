b5d4532016665f53399a0ca55542352299d35ae4
KyawThiha <kokyawthihanaingoo@gmail.com>
2025-11-07 22:52:56 +0630

    Initial Commit

---
+++ swift_ui_ios_widgets/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift_ui_ios_widgets
+//
+//  Created by Kyaw Thiha on 07/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift_ui_ios_widgets/swift_ui_ios_widgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_ios_widgetsApp.swift
+//  swift_ui_ios_widgets
+//
+//  Created by Kyaw Thiha on 07/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_ios_widgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
